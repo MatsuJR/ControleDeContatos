3894c4e77e5e4a114d339df51339f757292815aa
MatsuJR <dev.juniormatsushita@gmail.com>
2024-02-07 08:32:59 -0300

    Initial commit

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteConfirmation()
+        {
+            return View();
+        }
+
+
+    }
+}
